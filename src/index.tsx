c444cad04b7a000af6e634163a9cee9be1ec1835
kiwi-ui <yusnianggara@gmail.com>
2023-08-09 10:25:32 +0700

    style: add bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
